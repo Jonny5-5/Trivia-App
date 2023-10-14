20d67ac91a55284ecd0917031c7ad71eacf19041
Jonathan de Gaston <developerjon5@gmail.com>
2023-10-14 10:42:27 -0600

    Working single - player screen

--- lib/screens/single_play_screen.dart
+++ lib/screens/single_play_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:trivia_app/data/trivia_q.dart';
 import 'package:trivia_app/widgets/scaffold_container.dart';
+import 'package:trivia_app/widgets/trivia/trivia.dart';
 
 class SinglePlayScreen extends StatefulWidget {
   const SinglePlayScreen({super.key});
@@ -9,10 +11,46 @@ class SinglePlayScreen extends StatefulWidget {
 }
 
 class SinglePlayScreenState extends State<SinglePlayScreen> {
+  Questions questions = Questions();
+  Q q = Questions().getQuestion();
+
+  /// Refreshes the question
+  void refresh() {
+    q = questions.getQuestion();
+    setState(() {});
+  }
+
+  /// This is called when the user clicks on an answer
+  void onAnswer(String answer) {
+    if (answer == q.correctAnswer) {
+      debugPrint("You got the answer right");
+    } else {
+      debugPrint("Wrong answer");
+    }
+    refresh();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MyScaffoldContainerBackground(
-      child: Text("Single-Player Page"),
+      showAppBar: true,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Spacer(),
+          Trivia(
+            question: q,
+            answers: [q.correctAnswer!, ...q.incorrectAnswers!],
+            onAnswer: onAnswer,
+          ),
+          const Spacer(flex: 10),
+          ElevatedButton(
+            onPressed: refresh,
+            child: Text("refresh"),
+          ),
+          const Spacer(),
+        ],
+      ),
     );
   }
 }
