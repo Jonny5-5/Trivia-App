2fbaa549f6cc5127115b833f7961a6e49472dec8
jay <jaydearden88@gmail.com>
2023-10-14 10:55:30 -0600

    Update settings_screen.dart

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -8,12 +8,49 @@ class SettingsScreen extends StatefulWidget {
   State<SettingsScreen> createState() => SettingsScreenState();
 }
 
+enum difficulty {
+  easy, medium, hard, all
+}
+
+
 class SettingsScreenState extends State<SettingsScreen> {
+  bool soundOn = true;
+  difficulty currentDifficulty = difficulty.all;
+  void moveToNextDifficulty() {
+  int nextIndex = (currentDifficulty.index + 1) % difficulty.values.length;
+  currentDifficulty = difficulty.values[nextIndex];
+  setState(() {
+  
+  });
+}
   @override
   Widget build(BuildContext context) {
     return MyScaffoldContainerBackground(
       showAppBar: true,
-      child: Text("Settings Page"),
+      child:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: soundOn ? Colors.green : Colors.red),
+            onPressed: () {
+              setState(() {
+                soundOn = !soundOn;
+              });
+            },
+            child: const Text("Sounds"),
+          ),
+          ElevatedButton(
+            onPressed: moveToNextDifficulty,
+            child: const Text("Difficulty"),
+          ),
+          Text(
+            'Current difficulty is ${currentDifficulty.toString().split('.').last}',
+            style: TextStyle(fontSize: 20)
+            )
+        ],
+      )
+
     );
   }
 }
