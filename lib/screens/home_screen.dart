65ff3135ebdf47153b16d5050aa246239b3caa8b
Jonathan de Gaston <developerjon5@gmail.com>
2023-10-14 10:02:49 -0600

    Buttons on main screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:trivia_app/screens/group_play_screen.dart';
+import 'package:trivia_app/screens/single_play_screen.dart';
 import 'package:trivia_app/widgets/scaffold_container.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,11 +11,39 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
+  /// Go to the Single Player Screen
+  void onPressedSingle() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const SinglePlayScreen()),
+    );
+  }
+
+  /// Go to the Multi Player Screen
+  void onPressedMulti() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const GroupPlayScreen()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MyScaffoldContainerBackground(
       showAppBar: true,
-      child: Text("HOME SCREEN"),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: onPressedSingle,
+            child: const Text("Single Player"),
+          ),
+          ElevatedButton(
+            onPressed: onPressedMulti,
+            child: const Text("Multi Player"),
+          ),
+        ],
+      ),
     );
   }
 }
