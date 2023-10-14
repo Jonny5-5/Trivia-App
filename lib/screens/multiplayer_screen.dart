21b8dc707fce8dc520d182bb6ab90354fe399165
Jonathan de Gaston <developerjon5@gmail.com>
2023-10-14 10:44:46 -0600

    rename to multiplayer

--- lib/screens/multiplayer_screen.dart
+++ lib/screens/multiplayer_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:trivia_app/widgets/scaffold_container.dart';
 
-class GroupPlayScreen extends StatefulWidget {
-  const GroupPlayScreen({super.key});
+class MultiPlayerScreen extends StatefulWidget {
+  const MultiPlayerScreen({super.key});
 
   @override
-  State<GroupPlayScreen> createState() => GroupPlayScreenState();
+  State<MultiPlayerScreen> createState() => MultiPlayerScreenState();
 }
 
-class GroupPlayScreenState extends State<GroupPlayScreen> {
+class MultiPlayerScreenState extends State<MultiPlayerScreen> {
   @override
   Widget build(BuildContext context) {
     return MyScaffoldContainerBackground(
